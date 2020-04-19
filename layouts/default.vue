80898c6da2646e8759232268ab7aa0ff2e2fd8ba
Matthew Young <matthewyoung4@mail.weber.edu>
2020-04-18 18:37:35 -0600

    first push

---
+++ layouts/default.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-app>
+    <no-ssr>
+      <notifications group="notify" />
+    </no-ssr>
+    <v-content>
+      <v-toolbar color="indigo" dark>
+        <v-toolbar-side-icon></v-toolbar-side-icon>
+        <v-toolbar-title>
+          <n-link to="/">Learning Nuxt</n-link>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn flat>
+            <n-link to="/weather-app">Weather App</n-link>
+          </v-btn>
+          <v-btn flat>
+            <n-link to="/quiz">Quiz App</n-link>
+          </v-btn>
+          <!-- <v-btn v-if="$auth()" flat @click="logout">
+            Logout
+          </v-btn> -->
+        </v-toolbar-items>
+      </v-toolbar>
+      <transition>
+        <nuxt />
+      </transition>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout() {
+      this.$cookies.remove('token')
+      this.$store.commit('auth/setLoggedIn', false)
+      this.$router.push('/admin/login')
+    }
+  }
+}
+</script>
+
+<style>
+a {
+  text-decoration: none;
+  color: white;
+}
+</style>
